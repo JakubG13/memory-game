30a691066f9ad725ebbd78e9ad81c085f91f5867
Jakub Gaj <gaju2001@o2.pl>
2023-04-15 17:41:04 +0200

    add protection to spam clicks

--- js/game.js
+++ js/game.js
@@ -4,37 +4,44 @@ let cardTxt1 = "";
 let cardTxt2 = "";
 let card1 = "";
 let card2 = "";
-const play = () => {
-  cards.forEach((card) => {
-    card.addEventListener("click", () => {
-      if (number === 0) {
-        cardTxt1 = card.textContent;
-        card1 = card;
-      } else if (number === 1) {
-        cardTxt2 = card.textContent;
-        card2 = card;
-      }
+let flag = true;
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    if (number === 0) {
+      cardTxt1 = card.textContent;
+      card1 = card;
+    } else if (number === 1) {
+      cardTxt2 = card.textContent;
+      card2 = card;
+    }
 
-      if (number < 2) {
-        card.style.opacity = 1;
-        number++;
-      }
-      setTimeout(() => {
-        if (cardTxt1 !== cardTxt2) {
-          card.style.opacity = 0;
-        } else {
-          card1.style.opacity = 1;
-          card1.style.backgroundColor = "limegreen";
-          card2.style.opacity = 1;
-          card2.style.backgroundColor = "limegreen";
-        }
-      }, 2000);
-      if (number === 2) {
-        setTimeout(() => {
-          number = 0;
-        }, 2000);
-      }
-    });
+    if (number < 2) {
+      card.style.opacity = 1;
+      number++;
+    }
+    if (number === 2 && flag === true) {
+      play();
+    }
   });
+});
+
+const play = () => {
+  flag = false;
+  setTimeout(() => {
+    if (cardTxt1 !== cardTxt2) {
+      card1.style.opacity = 0;
+      card2.style.opacity = 0;
+    } else if (cardTxt1 === cardTxt2) {
+      card1.style.opacity = 1;
+      card1.style.backgroundColor = "limegreen";
+      card2.style.opacity = 1;
+      card2.style.backgroundColor = "limegreen";
+    }
+    number = 0;
+    cardTxt1 = "";
+    cardTxt2 = "";
+    card1 = "";
+    card2 = "";
+    flag = true;
+  }, 2000);
 };
-play();
